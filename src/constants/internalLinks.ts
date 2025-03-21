a1c2925d87fe72c3173dc5697200c3379b054ea2
Alexandr Sekerin <blazzed21@gmail.com>
2025-03-21 18:44:22 +0300

    feat: create `internalLinks` in constants

---
+++ src/constants/internalLinks.ts
@@ -0,0 +1,22 @@
+export const internalLinks = [
+	{
+		href: "#about",
+		name: "About me",
+	},
+	{
+		href: "#education",
+		name: "Education",
+	},
+	{
+		href: "#skills",
+		name: "My stack",
+	},
+	{
+		href: "#portfolio",
+		name: "Portfolio",
+	},
+	{
+		href: "#footer",
+		name: "Contacts",
+	},
+];
