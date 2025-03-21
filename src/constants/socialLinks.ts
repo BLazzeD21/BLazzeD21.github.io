939196306f14c6baf11ce6380341ad08d9b2dd54
Alexandr Sekerin <blazzed21@gmail.com>
2025-03-21 19:03:39 +0300

    fix: remove indexes from `socialLinks`

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,41 +1,35 @@
 export const socialLinks = [
 	{
-		id: "3bd7d83c-ab16-4618-9907-d4c613abe1d8",
 		link: "https://github.com/BLazzeD21",
 		name: "github",
 		asideIcon: "/icons/social/github.svg",
 		contactsIcon: "/icons/footer/github.svg",
 	},
 	{
-		id: "cb72b76b-ed17-474d-8864-3682f184daa7",
 		link: "https://vk.com/blazzed",
 		name: "vk",
 		asideIcon: "/icons/social/vk.svg",
 		contactsIcon: "/icons/footer/vk.svg",
 	},
 	{
-		id: "cb61a231-85aa-46c8-8569-2f9c1b8bd99b",
 		link: "https://www.instagram.com/sekerin21",
 		name: "instagram",
 		asideIcon: "/icons/social/instagram.svg",
 		contactsIcon: "/icons/footer/instagram.svg",
 	},
 	{
-		id: "94ec85ed-dac7-4aa5-a655-04eafa5a169e",
 		link: "https://t.me/BlazzeD21",
 		name: "telegram",
 		asideIcon: "/icons/social/telegram.svg",
 		contactsIcon: "/icons/footer/telegram.svg",
 	},
 	{
-		id: "159e6712-31c8-476c-b95f-e1fb4e0f64eb",
 		link: "https://www.linkedin.com/in/blazzed21/",
 		name: "linkedin",
 		asideIcon: "/icons/social/linkedin.svg",
 		contactsIcon: "/icons/footer/linkedin.svg",
 	},
 	{
-		id: "5345353453564eb",
 		link: "https://x.com/blazzed21",
 		name: "x",
 		asideIcon: "/icons/social/x.svg",
