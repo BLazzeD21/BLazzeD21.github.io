8cb5698fab696ebfedff6134d2323c21d101455e
Alexandr Sekerin <blazzed21@gmail.com>
2025-03-23 17:03:47 +0300

    feat: update icons for education and add a new item

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -2,13 +2,13 @@ import { EducationType } from "@/types";
 
 export const higherEducation: EducationType[] = [
 	{
-		imageUrl: "icons/experience/designer.svg",
+		imageUrl: "icons/education/college.svg",
 		name: "The St. Petersburg University of Management Technologies and Economics (UMTE)",
 		location: "Secondary professional education | St. Petersburg (2020-2023)",
 		body: "Specialty: 09.02.05 Applied Informatics (by industry).<br />Qualified as a technician-programmer.",
 	},
 	{
-		imageUrl: "icons/experience/university.svg",
+		imageUrl: "icons/education/university.svg",
 		name: "The St. Petersburg University of Management Technologies and Economics (UMTE)",
 		location: "Higher education | St. Petersburg (2023-2027)",
 		body: "Specialty: 09.03.03 Applied Informatics in Economics.<br />Qualified as a technician-programmer.",
@@ -17,9 +17,15 @@ export const higherEducation: EducationType[] = [
 
 export const additionalEducation: EducationType[] = [
 	{
-		imageUrl: "icons/experience/courses.svg",
+		imageUrl: "icons/education/courses.svg",
 		name: "Center for Continuing Education",
 		location: "Diploma of additional education | St. Petersburg (2015-2020)",
 		body: "Successfully completed training in the direction: information and communication technologies (ICT), Fundamentals of Object-Oriented Programming, Algorithmization and programming technologies, Basics of building databases based on MS SQL Server, ADO.NET, Developing Windows Applications in C#, Website layout and web design technologies.",
+	},
+	{
+		imageUrl: "icons/education/project.svg",
+		name: "Tomsk State University (TSU)",
+		location: "Diploma of Digital Departments | Tomsk (2024-2025)",
+		body: "Acquired project management skills, including the use of Agile methodologies: Scrum, Kanban in the context of the digital economy and to support IT projects in companies.",
 	},
 ];
