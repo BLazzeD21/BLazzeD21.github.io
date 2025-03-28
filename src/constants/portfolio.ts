6b5b6fa2d308021ebf7afa4f82d4594d3c420451
Aleksandr Sekerin <blazzed21@gmail.com>
2025-03-28 13:33:20 +0300

    feat: remove obsolete works

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -2,24 +2,6 @@ import { PortfolioItemType } from "@/types";
 
 export const portfolioItems: PortfolioItemType[] = [
 	{
-		githubLink: "https://github.com/BLazzeD21/Figma-UBER",
-		projectLink: "https://uber-partners.vercel.app",
-		imageSrc: "/img/portfolio/uber.jpg",
-		altText: "uber",
-	},
-	{
-		githubLink: "https://github.com/BLazzeD21/Figma-Pulse",
-		projectLink: "https://run-smart-pulse.vercel.app",
-		imageSrc: "/img/portfolio/pulse.jpg",
-		altText: "pulse",
-	},
-	{
-		githubLink: "https://github.com/BLazzeD21/Figma-NanoCard",
-		projectLink: "https://nano-card.vercel.app/",
-		imageSrc: "/img/portfolio/nanocard.jpg",
-		altText: "nanocard",
-	},
-	{
 		githubLink: "https://github.com/BLazzeD21/React-Eco-Trails",
 		projectLink: "https://eco-trails.vercel.app/",
 		imageSrc: "/img/portfolio/ecotrails.jpg",
@@ -29,8 +11,8 @@ export const portfolioItems: PortfolioItemType[] = [
 	},
 	{
 		altText: "soon",
-		gridColumn: "1",
-		gridRow: "2/4",
+		gridColumn: "2/4",
+		gridRow: "2",
 	},
 	{
 		githubLink: "https://github.com/BLazzeD21/Node.js-ChatGPT-Bot",
@@ -38,7 +20,7 @@ export const portfolioItems: PortfolioItemType[] = [
 		imageSrc: "/img/portfolio/openai.jpg",
 		altText: "chatgpt",
 		gridColumn: "2/4",
-		gridRow: "2",
+		gridRow: "1",
 	},
 	{
 		githubLink: "https://github.com/BLazzeD21/React-dodo-pizza",
@@ -48,8 +30,5 @@ export const portfolioItems: PortfolioItemType[] = [
 	},
 	{
 		altText: "soon",
-	},
-	{
-		altText: "soon",
-	},
+	}
 ];
